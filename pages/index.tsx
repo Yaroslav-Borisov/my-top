e4a53a46096c866056d770d2672c542fddf1754e
Yaroslav-Borisov <yarpolar@gmail.com>
2024-01-29 21:35:34 +0300

    Добавит новые компоненты

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Button, Htag, Ptag } from '@/components'
+import { Button, Htag, Ptag, Rating } from '@/components'
+import { Tag } from '@/components/Tag/Tag'
+import { useState } from 'react';
+import { withLayout } from '@/layout/Layout';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+function Home() {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
-			<Htag tag='h1'>Текст</Htag>
+			<Htag tag='h1'>Заголовок</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
       <Ptag size='l'>Большой</Ptag>
 			<Ptag>Средний</Ptag>
 			<Ptag size='s'>Маленький</Ptag>
+      <Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   )
 }
+
+export default withLayout(Home)
